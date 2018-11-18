df718337e70da35fbb6634af092cf9f2b2f1890d
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2018-11-18 12:48:29 -0500

    Add bit-shift operations.

--- wider.h
+++ wider.h
@@ -60,6 +60,43 @@ struct Wider {
         return cf;
     }
 
+    friend Wider& operator<<=(Wider& x, int y) {
+#ifdef ASSERTS
+        if (!(0 <= y && y < bit_width)) __builtin_unreachable();
+        if (y & -(1u << bit_width)) __builtin_unreachable();
+#endif
+        if (y >= bit_width/2) {
+            y &= (bit_width/2 - 1);
+            x.hi = x.lo << y;
+            x.lo = Int64{0};
+        } else if (y) {
+            int shift = (bit_width/2 - y) % (bit_width/2);
+            Int64 temp = (x.lo >> shift);
+            x.lo <<= y;
+            x.hi <<= y;
+            x.hi |= temp;
+        }
+        return x;
+    }
+    friend Wider& operator>>=(Wider& x, int y) {
+#ifdef ASSERTS
+        if (!(0 <= y && y < bit_width)) __builtin_unreachable();
+        if (y & -(1u << bit_width)) __builtin_unreachable();
+#endif
+        if (y >= bit_width/2) {
+            y &= (bit_width/2 - 1);
+            x.lo = x.hi >> y;
+            x.hi = Int64{0};
+        } else if (y) {
+            int shift = (bit_width/2 - y) % (bit_width/2);
+            Int64 temp = (x.hi << shift);
+            x.hi >>= y;
+            x.lo >>= y;
+            x.lo |= temp;
+        }
+        return x;
+    }
+
     friend Wider& operator+=(Wider& x, const Wider& y) { (void)producecarry(x, y); return x; }
     friend Wider& operator-=(Wider& x, const Wider& y) { (void)produceborrow(x, y); return x; }
     friend Wider& operator^=(Wider& x, const Wider& y) { x.lo ^= y.lo; x.hi ^= y.hi; return x; }
@@ -70,6 +107,8 @@ struct Wider {
     friend Wider operator^(Wider x, const Wider& y) { x ^= y; return x; }
     friend Wider operator&(Wider x, const Wider& y) { x &= y; return x; }
     friend Wider operator|(Wider x, const Wider& y) { x |= y; return x; }
+    friend Wider operator<<(Wider x, int y) { x <<= y; return x; }
+    friend Wider operator>>(Wider x, int y) { x >>= y; return x; }
 
     friend bool operator<(Wider x, const Wider& y) { return produceborrow(x, y); }
     friend bool operator>(const Wider& x, const Wider& y) { return (y < x); }
@@ -93,10 +132,14 @@ struct Tests {
     //static void minus(T *p, const T *q)      { *p = *p - *q; }
     //static void xoreq(T *p, const T *q)      { *p ^= *q; }
     //static void xor_(T *p, const T *q)       { *p = *p ^ *q; }
-    //static void oreq(T *p, const T *q)       { *p |= *q; }
-    //static void or_(T *p, const T *q)        { *p = *p | *q; }
     //static void andeq(T *p, const T *q)      { *p &= *q; }
     //static void and_(T *p, const T *q)       { *p = *p & *q; }
+    //static void oreq(T *p, const T *q)       { *p |= *q; }
+    //static void or_(T *p, const T *q)        { *p = *p | *q; }
+    //static void shleq(T *p, int q)           { *p <<= q; }
+    //static void shl(T *p, int q)             { *p = *p << q; }
+    //static void shreq(T *p, int q)           { *p >>= q; }
+    //static void shr(T *p, int q)             { *p = *p >> q; }
     //static bool lt(const T *p, const T *q)   { return *p < *q; }
     //static bool leq(const T *p, const T *q)  { return *p <= *q; }
     //static bool gt(const T *p, const T *q)   { return *p > *q; }
